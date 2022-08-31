fae1fcd3784daf122e2adc741bd2bd09c844c02e
Denis_Filimonchik_850505 <denis14philimoncik@gmail.com>
2022-08-31 09:05:45 +0300

    Added pagination on home page

--- home.php
+++ home.php
@@ -9,35 +9,33 @@ get_template_part('templates/headers/header-wrapper'); ?>
             <?php
             // циклы вывода записей
             // если записи найдены
-            if (have_posts()) {
-                while (have_posts()) {
-                    the_post();    ?>
+            while (have_posts()) {
+                the_post();    ?>
 
-                    <!-- 3 -->
-                    <div class="gallery-item">
-                        <div class="grid-item-holder">
-                            <article>
-                                <ul class="blog-title">
-                                    <li><a href="#" class="tag"><?php the_time('d M Y'); ?></a></li>
-                                    <li> - </li>
-                                    <li><a href="#" class="tag"><?php the_category(); ?></a></li>
-                                </ul>
-                                <div class="blog-media">
-                                    <?php the_post_thumbnail(); ?>
-                                </div>
-                                <div class="blog-text">
-                                    <h3><?php the_title(); ?></h3>
-                                    <p>
-                                        <?php the_excerpt(); ?>
-                                    </p>
-                                    <a href="<?php the_permalink(); ?>" class="ajax btn anim-button   trans-btn   transition  fl-l" target="_blank"><span>read more</span><i class="fa fa-eye"></i></a>
-                                </div>
-                            </article>
-                        </div>
+                <!-- 3 -->
+                <div class="gallery-item">
+                    <div class="grid-item-holder">
+                        <article>
+                            <ul class="blog-title">
+                                <li><a href="#" class="tag"><?php the_time('d M Y'); ?></a></li>
+                                <li> - </li>
+                                <li><a href="#" class="tag"><?php the_category(); ?></a></li>
+                            </ul>
+                            <div class="blog-media">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <div class="blog-text">
+                                <h3><?php the_title(); ?></h3>
+                                <p>
+                                    <?php the_excerpt(); ?>
+                                </p>
+                                <a href="<?php the_permalink(); ?>" class="ajax btn anim-button   trans-btn   transition  fl-l" target="_blank"><span>read more</span><i class="fa fa-eye"></i></a>
+                            </div>
+                        </article>
                     </div>
+                </div>
 
             <?php
-                }
             }
             ?>
 
@@ -46,12 +44,22 @@ get_template_part('templates/headers/header-wrapper'); ?>
         <!-- pagination   -->
         <div class="pagination-blog">
             <div class="pagination-blog-inner">
-                <a href="#" class="prevposts-link transition"><i class="fa fa-chevron-left"></i></a>
-                <a href="#" class="blog-page transition">1</a>
-                <a href="#" class="blog-page current-page transition">2</a>
-                <a href="#" class="blog-page transition">3</a>
-                <a href="#" class="blog-page transition">4</a>
-                <a href="#" class="nextposts-link transition"><i class="fa fa-chevron-right"></i></a>
+                <?php
+                global $wp_query;
+
+                $args = array(
+                    'base'    => get_pagenum_link(1) . '%_%',
+                    'format'  => '/page/%#%',
+                    'current' => max(1, get_query_var('paged')),
+                    'total'   => $wp_query->max_num_pages,
+                    'prev_text'    => __('<'),
+                    'next_text'    => __('>'),
+                );
+
+                $result = paginate_links($args);
+
+                echo $result;
+                ?>
             </div>
         </div>
         <!--  pagination end -->
