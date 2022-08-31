6b14bac83a1ca2c626efd42fc2fdbce896db930b
Denis_Filimonchik_850505 <denis14philimoncik@gmail.com>
2022-08-31 09:09:03 +0300

    Added using contact fields

--- contact.php
+++ contact.php
@@ -16,7 +16,7 @@ get_template_part('templates/headers/header-wrapper'); ?>
                     <h3 class="dec-text"><?php the_title(); ?></h3>
                     <p><?php the_content(); ?></p>
                     <a href="#" class=" btn anim-button   trans-btn   transition  fl-l showform"><span>Write us</span><i class="fa fa-eye"></i></a>
-                </div>      
+                </div>
             </section>
         </div>
         <!--  align-content  end-->
@@ -27,17 +27,7 @@ get_template_part('templates/headers/header-wrapper'); ?>
                 <section>
                     <div id="contact-form">
                         <div id="message"></div>
-                        <form method="post" action="php/contact.php" name="contactform" id="contactform">
-                            <input name="name" type="text" id="name" onClick="this.select()" value="Name">
-                            <input name="email" type="text" id="email" onClick="this.select()" value="E-mail">
-                            <input type="text" name="phone" id="phone" onClick="this.select()" value="Phone" />
-                            <textarea name="comments" id="comments" onClick="this.select()">Message</textarea>
-                            <div class="verify-wrap">
-                                <span class="verify-text"> How many gnomes were in the story about the "Snow-white" ?</span>
-                                <input name="verify" type="text" id="verify" onClick="this.select()" value="0">
-                            </div>
-                            <button type="submit" id="submit"><span>Send </span> <i class="fa fa-long-arrow-right"></i></button>
-                        </form>
+                        <?php echo do_shortcode('[contact-form-7 id="196" title="Без названия"]'); ?>
                     </div>
                 </section>
             </div>
